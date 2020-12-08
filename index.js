210227fa8b6e7ae94c7dbbd9d53b36fbd426d98c
Jolie Hobloss <jolieshobloss@gmail.com>
2020-12-08 11:52:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 let divide = function() {
   return 2000/100;
 }
-
 let square=x=>x**x
-
 let add = (a, b) => a + b
 
-/*describe("index.js", () => {
-
-
-  it("has an arrow function called add", () => {
-    expect(add).to.exist
-  })
-
-  it("add arrow function takes two parameters and adds them together", () => {
-    let a = 3
-    let b = 4
-    expect(add(a,b)).to.equal(a+b)
-  })
-})*/
